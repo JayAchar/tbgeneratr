d59a5012ebc7ef33dba3b42d60f512fbeebc2387
JA-OSX <jay.achar@doctors.org.uk>
2018-07-18 23:42:03 +0200

    updated age_generator()

--- R/age_generator.R
+++ R/age_generator.R
@@ -3,8 +3,9 @@
 #' Use date of birth and date of starting treatment to calculate 
 #' age at baseline. Takes data frame with date of birth and start 
 #' of treatment date to generate age variable in years. 
-#' @param x data frame containing Koch 6 admission variables
-#' @param db define database being used - "k6", "epi_info"
+#' @param x data frame containing Koch 6 admission variables#' 
+#' @param software define software used for data collection.
+#' Values can be "excel", "koch_6", "epiinfo"
 #' @param rm_orig remove original variables - TRUE or FALSE
 #' @author Jay Achar \email{jay.achar@@doctors.org.uk}
 #' @seealso \code{\link{tbgeneratr}}
@@ -15,28 +16,23 @@
 #' age_generator(p, dob = "dateofbirth", start = "Starttre")
 #' }
 
-age_generator <- function(x, db = "k6", rm_orig = TRUE) {
-# acceptable values for "set" arg
-	s <- c("k6", "epi_info")
-
+age_generator <- function(x, software = c("epiinfo", "koch_6", "excel"), rm_orig = TRUE) {
 # check input
 	if (!(is.data.frame(x))) {
 			stop("input paramter, x, must be a data frame")
 	}
 
-# check db is within acceptable values
-	if (! db %in% s) {
-		stop("Specify db argument within specified values")
-	}
+# check all args
+	software <- match.arg(software)
 
 # =================================================================
-# set db specific variables 
-		if (db == "k6") {
+# set software specific variables 
+		if (software == "koch_6") {
 			dob <- "dateofbirth"
 			start <- "Starttre"
 				
 		}	
-		if (db == "epi_info") {
+		if (software == "epiinfo") {
 			dob <- "BIRTDATE"
 			start <- "STARTTRE"
 
@@ -63,5 +59,5 @@ age_generator <- function(x, db = "k6", rm_orig = TRUE) {
  		x[[dob]] <- NULL
  	}
 
-	return(x)
+x
 }
