f554821679d4cf1ae050a5d5d17d420869e1b256
JA-Win <jay.achar@doctors.org.uk>
2018-10-27 22:07:48 +0200

    improve test() for converter()

--- tests/testthat/test-converter.R
+++ tests/testthat/test-converter.R
@@ -1,29 +1,21 @@
 context("test-converter")
 library(tbgeneratr)
 
-# generate test data
+# load test data
+raw <- system.file("testdata", "culture_conv.rds", package="tbgeneratr") %>% 
+    readRDS() 
+
+
 # epiinfo - corect
-out <- structure(list(id = c("a", "a", "a", "b", "b", "b", "b", "b", 
-       "b", "b", "b"), samp_date = structure(c(17140, 17212, 17254, 
-         13843, 13895, 13901, 13920, 13930, 13957, 13991, 14020), class = "Date"), 
-        culture = structure(c(2L, 1L, 1L, 2L, 1L, 2L, 2L, 1L, 1L, 
-        1L, 1L), .Label = c("Negative", "Positive"), class = c("ordered", 
-        "factor")), STARTTRE = structure(c(17184, 17184, 17184, 13908, 
-        13908, 13908, 13908, 13908, 13908, 13908, 13908), class = "Date"), 
-        DATEN = structure(c(17256, 17256, 17256, 14214, 14214, 14214, 
-        14214, 14214, 14214, 14214, 14214), class = "Date"), smear = structure(c(NA_integer_, 
-        NA_integer_, NA_integer_, NA_integer_, NA_integer_, NA_integer_, 
-        NA_integer_, NA_integer_, NA_integer_, NA_integer_, NA_integer_
-        ), .Label = character(0), class = "factor")), row.names = c(NA, -11L), class = c("tbl_df", 
-        "tbl", "data.frame"))
+correct <- converter(raw, "culture", "epiinfo", "kk", "adm") 
 
-correct <- converter(out, "culture", "epiinfo", "kk", "adm") 
-
-    
     test_that("culture conversion dates correct", {
         expect_equal(ncol(correct), 2)
-        expect_equal(nrow(correct), 2)
+        expect_equal(nrow(correct), 3)
         expect_true(all(c("id", "cc_date") %in% names(correct)))
         expect_match(class(correct$cc_date), "Date")
         expect_equal(length(unique(correct$id)), nrow(correct))
+        expect_equal(length(unique(raw$id)), length(unique(correct$id)))
+        expect_equal(correct$cc_date[1], as.Date("2017-02-15"))
+        expect_equal(correct$cc_date[2], as.Date("2008-02-21"))
     })
