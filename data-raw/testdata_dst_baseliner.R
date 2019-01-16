8136e96a7f614fc98178e4ad1e6db8a7ff4bdc8c
JA-OSX <jay.achar@doctors.org.uk>
2019-01-16 22:38:12 +0100

    improved tests for dst_baseliner.epiinfo()

--- data-raw/testdata_dst_baseliner.R
+++ data-raw/testdata_dst_baseliner.R
@@ -2,114 +2,128 @@
 
 ## EpiInfo admission and lab
 ### Admission data
-epi_adm <- data.frame(APID = paste0("XYZ", 1:6), stringsAsFactors = F) %>% 
-  dplyr::mutate(STARTTRE = lubridate::dmy(c(rep("1/1/10", 6))),
-                indictor_dst = c("DS-TB", "Inh mono", "Inh mono", 
-                                 "pre-XDR SLI", "NA", "MDR-TB"))
+epi_adm <-
+  data.frame(APID = paste0("XYZ", 1:8), stringsAsFactors = F) %>%
+  dplyr::mutate(
+    STARTTRE = lubridate::dmy(c(rep("1/1/10", 8))),
+    indictor_dst = c(
+      "DSTB",
+      "Inh-mono",
+      "Inh-mono",
+      "pre-XDRTB (SLI)",
+      NA_character_,
+      "RRTB",
+      "XDRTB",
+      "DSTB"
+    )
+  )
 
 class(epi_adm) <- c(class(epi_adm), "epiinfo")
-saveRDS(epi_adm, "inst/testdata/baseline_dst_epi_adm.rds")
+saveRDS(epi_adm, "inst/testdata/dst_baseliner_epi_adm.rds")
                 
 
 ### Laboratory data
- 
+
+
 epi_lab <-
   structure(
     list(
-      APID = c("XYZ1", "XYZ2", "XYZ3", "XYZ4", "XYZ4",
-               "XYZ5", "XYZ6", "XYZ6"),
-      MICRLABN = c(
-        "09-0001",
-        "09-0002",
-        "09-0003",
-        "09-0004",
-        "09-0005",
-        "09-0006",
-        "09-0007",
-        "09-0008"
+      APID = c(
+        "XYZ1",
+        "XYZ2",
+        "XYZ3",
+        "XYZ4",
+        "XYZ4",
+        "XYZ5",
+        "XYZ6",
+        "XYZ6",
+        "XYZ7",
+        "XYZ8"
       ),
-      samp_date = structure(c(
-        14608,
-        14608, 14608, 14608, NA, 14608, 14558, 14608
-      ), class = "Date"),
+      samp_date = structure(
+        c(14608,
+          14608, 14608, 14608, NA, 14608, 14558, 14608, 14608, 14608),
+        class = "Date"
+      ),
       hain_inh = structure(
-        c(1L, 2L, 1L, NA, 1L, NA, NA, NA),
+        c(1L, 2L, 1L, NA, 1L, NA, NA, NA, 2L, NA),
         .Label = c("Sensitive",
                    "Resistant"),
         class = "factor"
       ),
       hain_rif = structure(
         c(1L,
-          1L, 1L, 2L, 1L, NA, NA, NA),
+          1L, 1L, 2L, 1L, NA, NA, NA, 2L, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       ),
       xpert_res = structure(
         c(2L, 2L, 1L,
-          1L, 1L, NA, 2L, 2L),
+          1L, 1L, NA, 2L, 2L, NA, 2L),
         .Label = c("Negative", "Positive"),
         class = "factor"
       ),
       xpert_rif = structure(
-        c(NA, 1L, 1L, 1L, 1L, NA, 1L, 2L),
+        c(NA, 1L, 1L, 1L, 1L, NA, 1L, 2L, NA, 1L),
         .Label = c("Not detected",
                    "Detected"),
         class = "factor"
       ),
       dst_p_rif = structure(
         c(1L,
-          1L, 1L, 1L, 1L, NA, NA, NA),
+          1L, 1L, 1L, 1L, NA, NA, NA, 1L, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       ),
       dst_p_inh = structure(
         c(1L, 1L, 2L,
-          1L, 1L, NA, NA, NA),
+          1L, 1L, NA, NA, NA, 2L, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       ),
       dst_p_pza = structure(
-        c(1L, 1L, 1L, 1L, 1L, NA, NA, NA),
+        c(1L, 1L, 1L, 1L, 1L, NA, NA, NA, 2L, NA),
         .Label = c("Sensitive",
                    "Resistant"),
         class = "factor"
       ),
       dst_p_eth = structure(
         c(1L,
-          1L, 1L, 1L, 1L, NA, NA, NA),
+          1L, 1L, 1L, 1L, NA, NA, NA, NA, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       ),
       dst_p_cm = structure(
         c(1L, 1L, 1L,
-          2L, 1L, NA, NA, NA),
+          2L, 1L, NA, NA, NA, 1L, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       ),
       dst_p_mfx = structure(
-        c(NA, 1L, 1L, 1L, 2L, NA, NA, NA),
+        c(NA, 1L, 1L, 1L, 2L, NA, NA, NA, NA, NA),
         .Label = c("Sensitive",
                    "Resistant"),
         class = "factor"
       ),
       dst_p_km = structure(
         c(1L,
-          2L, 1L, 1L, 1L, NA, NA, NA),
+          2L, 1L, 1L, 1L, NA, NA, NA, 2L, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       ),
       dst_p_ofx = structure(
         c(1L, 1L, 2L,
-          1L, 1L, NA, NA, NA),
+          1L, 1L, NA, NA, NA, 2L, NA),
         .Label = c("Sensitive", "Resistant"),
         class = "factor"
       )
     ),
-    row.names = c(NA,
-                  -8L), class = c("tbl_df", "tbl", "data.frame"))%>% 
-  dplyr::mutate(MICRLABN = paste0("09-000", 1:nrow(.))) %>% 
+    row.names = c(NA,-10L),
+    class = c("tbl_df", "tbl", "data.frame")
+  ) %>%
+  dplyr::mutate(MICRLABN = paste0("09-000", 1:nrow(.))) %>%
   dplyr::select(APID, MICRLABN, samp_date, dplyr::everything())
 
 class(epi_lab) <- c(class(epi_lab), "epiinfo")
-saveRDS(epi_lab, "inst/testdata/baseline_dst_epi_lab.rds")
+saveRDS(epi_lab, "inst/testdata/dst_baseliner_epi_lab.rds")
 
