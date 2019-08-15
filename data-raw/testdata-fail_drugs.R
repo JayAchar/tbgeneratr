1751fa6c1eb02634bf2557900c387b5b932c3f60
JA-desktop <JayAchar@users.noreply.github.com>
2019-08-15 22:33:20 +0200

    add fail_drugs() with unit testing

--- data-raw/testdata-fail_drugs.R
+++ data-raw/testdata-fail_drugs.R
@@ -29,7 +29,8 @@ new_data <- function(unique_id_int,
                        lzd_change = factor(lzd, levels = 1:2, labels = c("Start", "Stop")),
                        mfx_change = factor(mfx, levels = 1:2, labels = c("Start", "Stop")),
                        dlm_change = factor(NA_integer_),
-                       cfz_change = factor(NA_integer_))
+                       cfz_change = factor(NA_integer_),
+                       stringsAsFactors = FALSE)
   
   list(adm = adm,
        change = change)
@@ -38,7 +39,25 @@ new_data <- function(unique_id_int,
 records <- list(
   # Start - no stop
   list(1, c(20, 51, 100), c(1, NA_integer_, NA_integer_), c(1, NA_integer_, NA_integer_), 
-       c(1, NA_integer_, NA_integer_))
+       c(1, NA_integer_, NA_integer_)),
+  # Stop 3 drugs after 1 month
+  list(2, c(20, 30, 100), c(1, 2, NA_integer_), c(1, 2, NA_integer_), 
+       c(1, 2, NA_integer_)),
+  # Stop 1 drug after 20 days, then a second after 30 days
+  list(3, c(20, 30, 100), c(2, NA_integer_, NA_integer_), c(1, 2, NA_integer_), 
+       c(1, NA_integer_, NA_integer_)),
+  # Second stop 60 days after first
+  list(4, c(20, 80, 100), c(2, NA_integer_, NA_integer_), c(1, 2, NA_integer_), 
+       c(1, NA_integer_, NA_integer_)), 
+  # Second stop 60 days after first, then third 20 days after second
+  list(5, c(20, 80, 100), c(2, NA_integer_, NA_integer_), c(1, 2, NA_integer_), 
+       c(1, NA_integer_, 2)), 
+  # two groups of two stops within 30 days of each other
+  list(6, c(20, 40, 90, 100), c(2, NA_integer_, NA_integer_, 2), c(1, 2, NA_integer_, NA_integer_), 
+       c(1, NA_integer_, 2, NA_integer_)), 
+  # first double stop on same day, followed by stop x2 on different days
+  list(7, c(20, 80, 100), c(2, NA_integer_, NA_integer_), c(2, 2, NA_integer_), 
+       c(1, NA_integer_, 2))
 )
 
 ## construct formula to generate data frame
--- tests/testthat/test-fail_drugs.R
+++ tests/testthat/test-fail_drugs.R
@@ -14,8 +14,15 @@ testing_code <- quote({
   expect_true(all(! is.na(output$fail_drugs_dt[output$fail_drugs == 1L])))
   
   expect_true(output$fail_drugs[output[[1]] == "XYZ1"] == 0L)
-  
-  expect_true(output$fail_drugs_dt[output[[1]] == "XYZ2"] == as.Date("2010-04-01"))
+  expect_true(output$fail_drugs[output[[1]] == "XYZ2"] == 1L)
+  expect_true(output$fail_drugs[output[[1]] == "XYZ3"] == 1L)
+  expect_true(output$fail_drugs[output[[1]] == "XYZ4"] == 0L)
+  expect_true(output$fail_drugs[output[[1]] == "XYZ5"] == 1L)
+  expect_true(output$fail_drugs_dt[output[[1]] == "XYZ5"] == as.Date("2010-04-11"))
+  expect_true(output$fail_drugs[output[[1]] == "XYZ6"] == 1L)
+  expect_true(output$fail_drugs_dt[output[[1]] == "XYZ6"] == as.Date("2010-02-10"))
+  expect_true(output$fail_drugs[output[[1]] == "XYZ7"] == 1L)
+  expect_true(output$fail_drugs_dt[output[[1]] == "XYZ7"] == as.Date("2010-01-21"))
  
 })
 
